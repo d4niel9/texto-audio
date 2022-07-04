773bfa81b594191d735d0422bcf0a0adad8e64f8
d4niel9 <d4niel9.py@gmail.com>
2022-07-04 14:56:55 -0500

    v 3.0

---
+++ main.py
@@ -0,0 +1,17 @@
+from gtts import gTTS
+import shutil
+import pathlib
+from playsound import playsound
+
+path = pathlib.Path().absolute()
+name_file = input('name file') + '.mp3'
+texto = 'Soy el texto convertido en audio'
+
+def archivo(path, name_file, texto):
+    audio = gTTSS(texto, lang='es', tld='es')
+    audio.save(name_file)
+    path = str(path) + '/outputfile/'
+    shutil.move(archivo, path)
+    path += name_file
+    playsound(path)
+archivo(path=path, name_file=name_file, texto=texto)
